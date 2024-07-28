1606e7882e45b6da4370508be9628ddeb50075c5
Ali Mohammadi <alimohammadi.8773@gmail.com>
2024-07-28 17:58:28 +0330

    changes on admin panel

--- core/admin.py
+++ core/admin.py
@@ -10,12 +10,10 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    add_fieldsets = (
-        (
-            (None, {'fields': ('user_code','phone', 'password', 'is_active')}),
-            ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
-            ('Make this user an admin', {'fields': ('is_staff',)})
-        ),
+    fieldsets = (
+        (None, {'fields': ('user_code','phone', 'password', 'is_active')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
+        ('Make this user an admin', {'fields': ('is_staff',)})
     )
     list_display = ['user_code','phone', 'display_qr_code']
 
